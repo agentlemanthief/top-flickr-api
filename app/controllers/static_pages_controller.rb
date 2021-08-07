375f235c20057846f8f936d1830ca31bf01a7575
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-08-07 17:10:48 +0100

    Built simple search form in index view and added logic in StaticPagesController for index action calling Flickr API.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
-  def index
+  require 'flickr'
 
+  def index
+    flickr = Flickr.new
+    unless params[:search][:user_id].blank?
+      @photos = flickr.photos.search(user_id: params[:search][:user_id])
+    else
+      @photos = flickr.photos.getRecent
+    end
   end
 end
